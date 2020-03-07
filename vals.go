3423baa9420eb55a6c3ee762729895059c37bbb9
Ravi Shekhar Jethani <rsjethani@gmail.com>
2020-03-07 01:46:00 +0100

    added int,string,float64 and there list versions

--- vals.go
+++ vals.go
@@ -34,37 +34,15 @@ func numError(err error) error {
 	return err
 }
 
-type IntList []int
-
-func NewIntList(p *[]int) *IntList {
-	return (*IntList)(p)
-}
-
-func (il *IntList) Set(values ...string) error {
-	*il = make([]int, len(values))
-	for i, val := range values {
-		n, err := strconv.ParseInt(val, 0, strconv.IntSize)
-		(*il)[i] = int(n)
-		if err != nil {
-			return numError(err)
-		}
-	}
-	return nil
-}
-
-func (il *IntList) Get() interface{} { return (*[]int)(il) }
-
-func (il *IntList) String() string { return fmt.Sprint(*il) }
-
-// --- int value
+// Int type represents an int value
 type Int int
 
 func NewInt(p *int) *Int {
 	return (*Int)(p)
 }
 
-func (i *Int) Set(s ...string) error {
-	v, err := strconv.ParseInt(s[0], 0, strconv.IntSize)
+func (i *Int) Set(values ...string) error {
+	v, err := strconv.ParseInt(values[0], 0, strconv.IntSize)
 	if err != nil {
 		err = numError(err)
 	}
@@ -76,7 +54,30 @@ func (i *Int) Get() interface{} { return int(*i) }
 
 func (i *Int) String() string { return strconv.Itoa(int(*i)) }
 
-// -- string Value
+// IntList type representing a list of integer values
+type IntList []int
+
+func NewIntList(p *[]int) *IntList {
+	return (*IntList)(p)
+}
+
+func (il *IntList) Set(values ...string) error {
+	*il = make([]int, len(values))
+	for i, val := range values {
+		n, err := strconv.ParseInt(val, 0, strconv.IntSize)
+		if err != nil {
+			return numError(err)
+		}
+		(*il)[i] = int(n)
+	}
+	return nil
+}
+
+func (il *IntList) Get() interface{} { return (*[]int)(il) }
+
+func (il *IntList) String() string { return fmt.Sprint(*il) }
+
+// String type represents a string value and satisfies ArgValue interface
 type String string
 
 func NewString(p *string) *String {
@@ -92,7 +93,26 @@ func (s *String) Get() interface{} { return string(*s) }
 
 func (s *String) String() string { return string(*s) }
 
-// -- float64 Value
+// StringList type represents a list string value and satisfies ArgValue interface
+type StringList []string
+
+func NewStringList(p *[]string) *StringList {
+	return (*StringList)(p)
+}
+
+func (sl *StringList) Set(values ...string) error {
+	*sl = make([]string, len(values))
+	for i, val := range values {
+		(*sl)[i] = val
+	}
+	return nil
+}
+
+func (sl *StringList) Get() interface{} { return (*[]string)(sl) }
+
+func (sl *StringList) String() string { return fmt.Sprint(*sl) }
+
+// Float64 represents a float64 value and also satisfies ArgValue interface
 type Float64 float64
 
 func NewFloat64(p *float64) *Float64 {
@@ -111,3 +131,26 @@ func (f *Float64) Set(s ...string) error {
 func (f *Float64) Get() interface{} { return float64(*f) }
 
 func (f *Float64) String() string { return strconv.FormatFloat(float64(*f), 'g', -1, 64) }
+
+// Float64List type representing a list of float64 values and satisfies ArgValue interface
+type Float64List []float64
+
+func NewFloat64List(p *[]float64) *Float64List {
+	return (*Float64List)(p)
+}
+
+func (fl *Float64List) Set(values ...string) error {
+	*fl = make([]float64, len(values))
+	for i, val := range values {
+		f, err := strconv.ParseFloat(val, 64)
+		if err != nil {
+			return numError(err)
+		}
+		(*fl)[i] = float64(f)
+	}
+	return nil
+}
+
+func (fl *Float64List) Get() interface{} { return (*[]float64)(fl) }
+
+func (fl *Float64List) String() string { return fmt.Sprint(*fl) }
