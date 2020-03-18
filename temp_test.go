867a2fc569e6606de89151289989f6d2fe0a502c
Ravi Shekhar Jethani <rsjethani@gmail.com>
2020-03-18 19:43:10 +0100

    replace , with |

--- temp_test.go
+++ temp_test.go
@@ -39,14 +39,14 @@ func (p *point) IsBoolValue() bool { return false }
 
 func TestTraditionalApproach(t *testing.T) {
 	config := struct {
-		Salute   string  `argparser:"help=Salutation for the employee,name=salute"`
-		Salary   float64 `argparser:"help=Employee salary,pos=yes,name=salary"`
-		FullName string  `argparser:"pos=yes,name=full-name,help=Full name of the employee,pos=yes"`
-		EmpID    int     `argparser:"name=emp-id,help=Employee ID for new employee,short=i"`
-		Loc      point   `argparser:"name=point"`
-		IsIntern bool    `argparser:"name=is-intern"`
+		Salute   string  `argparser:"help=Salutation for the employee|name=salute"`
+		Salary   float64 `argparser:"help=Employee salary,pos=RRR|name=salary"`
+		FullName string  `argparser:"pos=yes|name=full-name|help=Full name of the employee|pos=yes"`
+		// EmpID    int     `argparser:"name=emp-id|help=Employee ID for new employee|short=i"`
+		// Loc      point   `argparser:"name=point"`
+		// IsIntern bool    `argparser:"name=is-intern"`
 	}{
-		EmpID:  -1,
+		// EmpID:  -1,
 		Salute: "Mr.",
 	}
 
@@ -57,9 +57,9 @@ func TestTraditionalApproach(t *testing.T) {
 	mainSet.Description = "CLI for managing employee database\n...\n..."
 
 	fmt.Printf("\nmainset: %+v\n", mainSet)
-	fmt.Println(mainSet.Usage())
+	// fmt.Println(mainSet.Usage())
 
-	fmt.Printf("\n%+v\n", config)
-	fmt.Println(mainSet.ParseFrom([]string{"34", "asd", "--is-intern", "--emp-id", "88888888888888888888888888888888"}))
-	fmt.Printf("\n%+v\n", config)
+	// fmt.Printf("\n%+v\n", config)
+	// fmt.Println(mainSet.ParseFrom([]string{"34", "asd", "--is-intern", "--emp-id", "88888888888888888888888888888888"}))
+	// fmt.Printf("\n%+v\n", config)
 }
