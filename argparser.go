9142b2ac81c5ff27c4beafbe96ba6c860b27feec
Ravi Shekhar Jethani <rsjethani@gmail.com>
2020-03-31 00:02:07 +0200

    User can now redirect Usage() output
    User can now configure output stream/writer by calling
    ArgParser.SetOutput(io.Writer)
    
    Addresses:

--- argparser.go
+++ argparser.go
@@ -2,26 +2,38 @@ package argparser
 
 import (
 	"fmt"
+	"io"
 	"os"
 )
 
 type ArgParser struct {
 	mainArgSet *ArgSet
+	usageOut   io.Writer
+}
+
+func (parser *ArgParser) SetOutput(w io.Writer) {
+	if w == nil {
+		parser.usageOut = os.Stderr
+		return
+	}
+	parser.usageOut = w
 }
 
 func NewArgParser(argSet *ArgSet) *ArgParser {
-	parser := &ArgParser{mainArgSet: argSet}
+	parser := &ArgParser{mainArgSet: argSet, usageOut: os.Stderr}
 	return parser
 }
 
 func (parser *ArgParser) Usage() {
-	fmt.Printf("Usage of %s:\n%s", os.Args[0], parser.mainArgSet.Usage())
+	fmt.Fprintf(parser.usageOut, "Usage of %s:\n", os.Args[0])
+	parser.mainArgSet.usage(parser.usageOut)
+	fmt.Fprintln(parser.usageOut, "")
 }
 
 func (parser *ArgParser) ParseFrom(args []string) error {
-	return parser.mainArgSet.ParseFrom(args)
+	return parser.mainArgSet.parseFrom(args)
 }
 
 func (parser *ArgParser) Parse() error {
-	return parser.ParseFrom(os.Args)
+	return parser.ParseFrom(os.Args[1:])
 }
